e7bfedbaa7695e96a55d15f6cd6a80526dd874f0
s2710099 <r.grgac@student.utwente.nl>
2023-10-10 15:54:12 +0200

    Delete the display from the wrong branch

--- hardware/display/display.py
+++
@@ -1,8 +0,0 @@
-from RPLCD import *
-from time import sleep
-from RPLCD.i2c import CharLCD
-lcd = CharLCD('PCF8574', 0x27) #this defines the name of the peripheral (lcd screen) and its address it is used when sending commands
-
-lcd.cursor_pos = (0, 0)
-lcd.write_string('Hello World')
-
