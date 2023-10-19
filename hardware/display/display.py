52a97d2abef1e46db13c45e431737973942f6359
Dany <d.shalhoub@student.utwente.nl>
2023-10-20 00:09:31 +0200

    Remocing unnecessary files and folders

--- hardware/display/display.py
+++
@@ -1,8 +0,0 @@
-from RPLCD import *
-from time import sleep
-from RPLCD.i2c import CharLCD
-lcd = CharLCD('PCF8574', 0x27) #this defines the name of the peripheral (lcd screen) and its address it is used when sending commands
-
-def screen_write(pointer_x,pointer_y,text):
-    lcd.cursor_pos = (pointer_x, pointer_y)
-    lcd.write_string(text)
--- hardware/keypad/test.py
+++
@@ -1,9 +0,0 @@
-x = 0
-
-def change_x (new_x):
-    global x
-    x = new_x
-
-change_x(2)
-
-print(x)
