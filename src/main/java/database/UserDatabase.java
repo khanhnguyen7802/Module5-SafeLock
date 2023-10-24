3ed0d555b2f898de6d709a08b62d8b91a5a09918
Shun Nishijima <syun136.616@gmail.com>
2023-10-24 15:56:05 +0200

    postgre connection

--- src/main/java/database/UserDatabase.java
+++ src/main/java/database/UserDatabase.java
@@ -1,62 +1,50 @@
 package database;
+
+import model.User;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import model.User;
+
 public class UserDatabase {
-        private final String jdbcURL = "jdbc:mysql://localhost:3306/db_name";///change it to exact url
-        private final String jdbcUsername = "username";
-        private final String jdbcPassword = "password";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/project";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Sairen1360!";
 
-        private static final String INSERT_USERS_SQL = "INSERT INTO users" + "  (username, password) VALUES " +
-                " (?, ?);";
-        private static final String SELECT_USER_BY_USERNAME = "select id,username,password from users where username =?";
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    }
 
-        public UserDatabase() {
-        }
-
-        protected Connection getConnection() {
-            Connection connection = null;
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-            } catch (SQLException | ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            return connection;
-        }
-
-        public void insertUser(User user) throws SQLException {
-            try (Connection connection = getConnection();
-                 PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
-                preparedStatement.setString(1, user.getName());
-                preparedStatement.setString(2, user.getPassword());
-                preparedStatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public User selectUser(String username) {
-            User user = null;
-            try (Connection connection = getConnection();
-                 PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_USERNAME)) {
-                preparedStatement.setString(1, username);
-                ResultSet rs = preparedStatement.executeQuery();
-
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String name = rs.getString("username");
-                    String password = rs.getString("password");
-                    user = new User();
-                    user.setName(name);
-                    user.setPassword(password);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return user;
+    public static void insertUser(User user) {
+        try (Connection connection = connect()) {
+            String query = "INSERT INTO users (username, password) VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getPassword());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
+
+    public static User selectUser(String username) {
+        User user = null;
+        try (Connection connection = connect()) {
+            String query = "SELECT * FROM users WHERE username = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String password = resultSet.getString("password");
+                user = new User(name, password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+}
