f592897d464b89d15e03e6020d4d9eb7bad3b59c
Samoes <s.s.f.mulder@student.utwente.nl>
2023-10-31 15:25:31 +0100

    Implemented GPS fetch

--- src/main/webapp/WebDevelopment/Functions/googleMaps.js
+++ src/main/webapp/WebDevelopment/Functions/googleMaps.js
@@ -5,7 +5,25 @@ async function initMap() {
     // The location of Uluru
     // fetch location
     console.log("Generating map");
-
+    let username = window.location.search;
+    username = username.split("?")[1];
+    let formData = new URLSearchParams();
+    formData.append('username', username);
+    fetch("http://localhost:8080/SAFEty/api/gps",
+        {
+            method: "POST",
+            body: formData,
+            headers: {
+                "Content-type" : "application/x-www-form-urlencoded"
+            }
+        }
+    ).then(
+        response => {
+            if (response.ok) {
+                window.location.href = "../index.html";
+            }
+        }
+    )
     const latitude = Math.random() * 170 - 85;
     console.log(latitude);
     const longitude = Math.random() * 360 - 180;
