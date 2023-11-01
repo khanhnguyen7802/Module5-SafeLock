8e6cd521e2fc0f3f9f7eaffb2833a7ae5cd6d24b
Shun Nishijima <syun136.616@gmail.com>
2023-11-01 17:33:33 +0100

    make some exception for update command

--- src/main/java/network/TCPServer.java
+++ src/main/java/network/TCPServer.java
@@ -27,15 +27,32 @@ public class TCPServer {
             if(inputData.startsWith("GPS")){
                 String[] splits = inputData.split("~");
                 String username = splits[1];
-                String newGPS = splits[2];
-                UserDatabase.updateGPS(username,newGPS);
-
+                char[] chars = username.toCharArray();
+                if ((chars[0] >= 'a' && chars[0] <= 'z') || (chars[0] >= 'A' && chars[0] <= 'Z')){
+                    String newGPS = splits[2];
+                    String point = newGPS.split(",")[0];
+                     if (!point.equals("")) {
+                         UserDatabase.updateGPS(username, newGPS);
+                     }else{
+                         System.out.println("the gps point is blank");
+                     }
+                }else{
+                    System.out.println("the username is not allowed");
+                }
             } else if (inputData.startsWith("UPDATE")) {
                 String[] splits = inputData.split("~");
                 String username = splits[1];
-                String newPassword = splits[2];
-                UserDatabase.updatePassword(username,newPassword);
-
+                char[] chars = username.toCharArray();
+                if ((chars[0] >= 'a' && chars[0] <= 'z') || (chars[0] >= 'A' && chars[0] <= 'Z')){
+                    String newPassword = splits[2];
+                    if (!newPassword.equals("")){
+                        UserDatabase.updatePassword(username,newPassword);
+                    }else{
+                        System.out.println("The password is blank");
+                    }
+                }else {
+                    System.out.println("The username is not allowed");
+                }
             }else {
                 System.out.println("Received data: " + inputData);
             }
