4f9a5a3ef0dd98ec4cea30f817575afb8e7f4a1b
Shun Nishijima <syun136.616@gmail.com>
2023-11-03 15:15:18 +0100

    redirect for Delete Brute

--- src/main/webapp/WebDevelopment/Functions/brute.js
+++ src/main/webapp/WebDevelopment/Functions/brute.js
@@ -14,6 +14,7 @@ function clearBruteTable() {
             console.log(data);
         })
         .catch(error => console.error('Error:', error));
+    window.location.href = window.location.pathname;
 }
 
 fetchBruteCount();
