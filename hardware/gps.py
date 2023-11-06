a21a4df772fc758c9dcef76f94baea4801a5b0ad
Dany <d.shalhoub@student.utwente.nl>
2023-11-06 14:13:07 +0100

    some merge errors occured, fixed it

--- hardware/gps.py
+++ hardware/gps.py
@@ -95,7 +95,6 @@ def power_down(power_key):
     time.sleep(18)
     print('finished')
     
-<<<<<<< HEAD
 try:
     init_gps(power_key)
     get_gps_position()
@@ -104,14 +103,4 @@ except:
     if ser != None:
         ser.close()
         power_down(power_key)
-=======
-def gps_final_working():
-    try:
-        init_gps(power_key)
-        get_gps_position()
-        power_down(power_key)
-    except:
-        if ser != None:
-            ser.close()
-            power_down(power_key)
->>>>>>> 47cf3eac0cb0cebd4d7f23e2ca7bba39e97d8e2b
+        
